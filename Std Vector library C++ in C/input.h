01d0b61331d2ec4752cc4789dea7e0f83fdc743e
DwivediAbhinav2008 <dwivediabhinav2008@gmail.com>
2026-01-03 14:39:57 +0530

    Add input.h for handling integer input

---
+++ Std Vector library C++ in C/input.h
@@ -0,0 +1,10 @@
+#ifndef INPUT_H
+#define INPUT_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include "flushinput.h"
+#include <string.h>
+void read_int(const char *prompt, int *out);
+
+#endif
